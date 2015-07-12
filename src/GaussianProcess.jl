1b03c552effd12519c3b48d1587ddaf103cc889b
linxihui <xihuil.silence@gmail.com>
2015-07-12 00:36:02 -0400

    module information

---
+++ src/GaussianProcess.jl
@@ -0,0 +1,23 @@
+module GaussianProcess
+	using DataFrames, Distributions, GLMNet
+
+	export 
+		gausspr, # Gaussian process classification, regression
+		predict, # prediction function for Gaussian process model
+		kernVanilla, # Vanilla / linear kernel
+		kernPoly, # polynomial kernel
+		kernRBF, # radial based / Gaussian kernel
+		kernLaplace, # Laplace kernel
+		kernMatern, # Matern class kernel
+		rowwiseDist, # calculate row-wise Euclidean distance
+		standardize, # standardize matrices
+		modelmatrix  # compute design matrix, response from Formula-DataFrame inputs
+
+	abstract GaussianProcessFitted # output of Gaussian process model
+
+	include("misc.jl")
+	include("kernel_rbf.jl")
+	include("modelmatrix.jl")
+	include("gaussian_process.jl")
+
+end
