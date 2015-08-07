33cd175f60d7081d8ea3c05b6f9a5ebf32fc39cf
linxihui <xihuil.silence@gmail.com>
2015-08-07 00:46:04 -0400

    changed field Real to Float64

--- src/kernels.jl
+++ src/kernels.jl
@@ -6,32 +6,32 @@ end
 
 typealias kernelLinear kernelVanilla
 
-kernelMatrix(K::kernelVanilla, x::Array{Real}, y::Array{Real} = x) = x*y.'
+kernelMatrix(K::kernelVanilla, x::Array, y::Array = x) = x*y.'
 
 # Polynomial kernel
 # 	(x'y + c)^d, where c = offect, d = degree
 immutable kernelPoly <: Kernel
     degree::Int
-    offset::Real
+    offset::Float64
     function kernelPoly(degree::Int = 3, offset::Real = 1)
         degree > 0 || error("degree must be positive.")
         new(degree, offset)
     end
 end
 
-kernelMatrix(K::kernelPoly, x::Array{Real}, y::Array{Real} = x) = (x*y.' .+ K.offset).^K.degree
+kernelMatrix(K::kernelPoly, x::Array, y::Array = x) = (x*y.' .+ K.offset).^K.degree
 
 # RBF kernel
 # 	exp(-d^2/(2*σ²)
 immutable kernelRBF <: Kernel
-	σ::Real
+	σ::Float64
 	function kernelRBF(σ::Real = 1.0)
 		σ > 0 || error("σ must be positive.")
 		new(σ)
 	end
 end
 
-function kernelMatrix(K::kernelRBF, x::Array{Real}, y::Array{Real} = x)
+function kernelMatrix(K::kernelRBF, x::Array, y::Array = x)
     exp(-rowwiseDist(x, y, squared = true) / (2*K.σ^2))
 end
 
@@ -59,7 +59,7 @@ immutable kernelMatern <: Kernel
 	end
 end
 
-function kernelMatrix(K::kernelMatern, x::Array{Number}, y::Array{Number} = x)
+function kernelMatrix(K::kernelMatern, x::Array, y::Array = x)
     d = (sqrt(2)*K.ν / K.σ)*rowwiseDist(x, y) + 1e-20*eye(size(x, 1));
     besselk(K.ν, d) .* (d .^ K.ν) / (gamma(K.ν) * 2.0^(K.ν - 1))
 end
@@ -67,11 +67,11 @@ end
 
 # User define kernel (matrix)
 immutable kernelUser <: Kernel
-	K::Matrix{Real}
-	function kernelUser(K::Matrix{Real})
+	K::Matrix
+	function kernelUser(K::Matrix)
 		isposdef(K) || error("Matrix must be positive definite.")
 		new(K)
 	end
 end
 
-kernelMatrix(K::kernelUser, x::Array{Number}, y::Array{Number} = x) = K.K
+kernelMatrix(K::kernelUser, x::Array, y::Array = x) = K.K
