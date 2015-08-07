fa2790bd5626ec5398d3355596d02d0408f5562c
linxihui <xihuil.silence@gmail.com>
2015-08-07 00:48:39 -0400

    update export objects

--- src/GaussianProcess.jl
+++ src/GaussianProcess.jl
@@ -1,16 +1,24 @@
 module GaussianProcess
-	using DataFrames, Distributions, GLMNet
+	using Docile, DataFrames, Distributions, GLMNet
 
+	import DataFrames.predict
 	export 
+        GaussianProcessFitted,
+        GaussianProcessFittedMatrix,
+        GaussianProcessFittedFormula,
 		gausspr, # Gaussian process classification, regression
 		predict, # prediction function for Gaussian process model
-		kernVanilla, # Vanilla / linear kernel
-		kernPoly, # polynomial kernel
-		kernRBF, # radial based / Gaussian kernel
-		kernLaplace, # Laplace kernel
-		kernMatern, # Matern class kernel
+        Kernel, # abstract kernel type
+		kernelVanilla, # Vanilla / linear kernel
+		kernelPoly, # polynomial kernel
+		kernelRBF, # radial based / Gaussian kernel
+		kernelLaplace, # Laplace kernel
+		kernelMatern, # Matern class kernel
 		rowwiseDist, # calculate row-wise Euclidean distance
+        Standardization, 
 		standardize, # standardize matrices
+        transform,  # apply Standardization object to new data
+        inverseTransform, # transform standardized data to original scale
 		modelmatrix  # compute design matrix, response from Formula-DataFrame inputs
 
 	abstract GaussianProcessFitted # output of Gaussian process model
